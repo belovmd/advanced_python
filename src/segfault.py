51750a223a3cee6174cbb6d1acdd0b61bd4c07ad
Maxim Belov <belovmd@gmail.com>
2018-08-31 20:44:43 +0300

    Task#2

---
+++ src/segfault.py
@@ -0,0 +1,7 @@
+"""Advanced python courses. Homework. Task #2. Maxim Belov."""
+
+import ctypes
+
+invalid_adress = 0
+ctypes.string_at(invalid_adress)
+print("It's very strange to see this message")
